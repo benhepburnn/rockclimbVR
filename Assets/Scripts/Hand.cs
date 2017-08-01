e15cdc2f4e029a2c42c6fc12e8169e83a982ce5c
ohjhesus <benjburn89@gmail.com>
2017-08-01 19:34:25 +1000

    first commit

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+	public bool leftHand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!leftHand) {
+//			transform.localPosition = OVRInput.GetLocalControllerPosition ();
+//			transform.localRotation = GetLocalControllerRotation ();
+		}
+	}
+}
